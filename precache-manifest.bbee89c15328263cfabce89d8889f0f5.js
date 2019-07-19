f6d2360c68f22f97d25beff1c263038d13cc6786
unknown <marcela.hdez.1801@gmail.com>
2019-07-19 09:39:51 -0500

    Updates

---
+++ precache-manifest.bbee89c15328263cfabce89d8889f0f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb48fcb523c8cc16d6d55c64b05c2665",
+    "url": "/gdl002-framework-social-network/index.html"
+  },
+  {
+    "revision": "0b847f1a074f87147359",
+    "url": "/gdl002-framework-social-network/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2ac98d149ab162699374",
+    "url": "/gdl002-framework-social-network/static/js/2.2bcd7024.chunk.js"
+  },
+  {
+    "revision": "0b847f1a074f87147359",
+    "url": "/gdl002-framework-social-network/static/js/main.0a9074a0.chunk.js"
+  },
+  {
+    "revision": "ac73f48bbab9e31e26c2",
+    "url": "/gdl002-framework-social-network/static/js/runtime~main.44a54b25.js"
+  }
+]);
